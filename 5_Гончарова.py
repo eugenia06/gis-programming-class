be3ed87f315dd004949c70d15416042869efa370
eugenia06 <dfgktf@gmail.com>
2025-04-03 20:23:09 +0300

    Update 5_Гончарова.py

--- 5_Гончарова.py
+++ 5_Гончарова.py
@@ -1,28 +1,30 @@
 from random import randint
 pr = QgsProject.instance()
-layer = pr.mapLayersByName("station")[0]
+#подключение исходных слоёв к коду(исходные слои открыты в qgis)
+layer = pr.mapLayersByName("station")[0] 
 poligons = pr.mapLayersByName("districts")[0]
-print(layer.featureCount())
-layer.setSubsetString('"colour" = \'orange\'')
-print(layer.featureCount())
+#print(layer.featureCount())
+layer.setSubsetString('"colour" = \'orange\'') #фильтрации по цвету ветки
+#print(layer.featureCount()) #проверка результата фильтрации
 station = QgsVectorLayer('Polygon?crs=EPSG:3857', "station_copy", "memory")
 prov = station.dataProvider()
 prov.addAttributes(layer.fields())
-station.updateFields()
-random_1 = randint(1, 100)
-b_d = (random_1 + 5) * 20
+station.updateFields() #обновление и сохранение данных на промежуточном слое
+
+random_1 = randint(1, 100) #получение рандомного числа от 1 до 99(включительно)
+b_d = (random_1 + 5) * 20 #получение радиуса для буферизации
+
 for stations in layer.getFeatures():
     n_f = QgsFeature()
     n_f.setGeometry(QgsGeometry.fromPointXY(stations.geometry().asPoint()).buffer(b_d, 8))
     n_f.setAttributes(stations.attributes())
     prov.addFeature(n_f)
-station.updateExtents()
-#pr.addMapLayer(station)
-layer.setSubsetString('')
-print(layer.featureCount())
-layer_with_intersects = QgsVectorLayer('Polygon?crs=EPSG:3857', "station/districts", "memory")
-prov_2 = layer_with_intersects.dataProvider()
-prov_2.addAttributes(poligons.fields())
+station.updateExtents() #обновление и сохранение данных на промежуточном слое
+#pr.addMapLayer(station) #для вывода результата фильтрации станций метро по цвету ветки
+layer.setSubsetString('') #снятие фильтрации с layer(исходный слой)
+#print(layer.featureCount()) #для проверки снятия фильтрации
+
+layer_with_intersects = QgsVectorLayer('Polygon?crs=EPSG:3857', "station/districts", "memory") #создание финального слоя с результатом пересечения 
 layer_with_intersects.updateFields()
 for poly in poligons.getFeatures():
     p_g = poly.geometry()
@@ -34,7 +36,7 @@ for poly in poligons.getFeatures():
             n_f_2.setAttributes(poly.attributes()) 
             prov_2.addFeature(n_f_2)
             break
-layer_with_intersects.updateExtents()
+layer_with_intersects.updateExtents() #обновление и сохранение данных на финальном слое
 pr.addMapLayer(layer_with_intersects)
 
 
